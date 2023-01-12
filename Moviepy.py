9f0709837681384d8305b0ff3f6f49c92ac9e5d1
Er Riyaz Ahmad Wani <74089872+erriyazwani@users.noreply.github.com>
2023-01-12 10:36:21 +0530

    Update Moviepy.py

--- Moviepy.py
+++ Moviepy.py
@@ -1,5 +1,9 @@
+this is not a script it is note 
 #movie was showing error  Need ffmpeg exe. You can download it by calling:
 #fix
 pip install imageio
 import imageio
 imageio.plugins.ffmpeg.download()
+
+for manully install --add to path
+#https://github.com/imageio/imageio-binaries/blob/master/ffmpeg/ffmpeg-win64-v4.2.2.exe
